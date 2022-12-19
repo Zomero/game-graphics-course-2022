638eb933472df3c8f3f8ebe3a9662f8892d7511b
zomero <solovovs.maria@gmail.com>
2022-12-19 23:47:08 +0200

    texturing homework

--- demos/texturing.js
+++ demos/texturing.js
@@ -103,7 +103,7 @@ async function loadTexture(fileName) {
     return await createImageBitmap(await (await fetch("images/" + fileName)).blob());
 }
 
-const tex = await loadTexture("abstract.jpg");
+const tex = await loadTexture("texture2.jpg");
 let drawCall = app.createDrawCall(program, vertexArray)
     .texture("tex", app.createTexture2D(tex, tex.width, tex.height, {
         magFilter: PicoGL.LINEAR,
